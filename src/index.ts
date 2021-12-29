1252a0f09196efc86f997042571b64f5a72bcdb9
JoakimEskils <joakimeskils@gmail.com>
2021-12-29 20:46:42 +0100

    init DB

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("hello world");
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+console.log("jek");
+console.log(process.env.DB_PASS);
+const main = async () => {
+    const orm = await MikroORM.init({
+        entities: [Post],
+        dbName: 'liserver',
+        type: 'postgresql',
+        debug: !__prod__,
+        user: 'postgres',
+        password: process.env.DB_PASS
+    });
+
+    const post = orm.em.create(Post, {title: 'my first post'})
+    await orm.em.persistAndFlush(post);
+}
+
+main();
